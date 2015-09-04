1f7531a27d67d8e17ad3641d79d8ea927f115525
Kristina Hager <kristina.hager@gmail.com>
2015-09-04 09:17:22 -0500

    show using library in this project

--- app/src/main/java/pimp/hellotospatialite/GeoDatabaseHandler.java
+++ app/src/main/java/pimp/hellotospatialite/GeoDatabaseHandler.java
@@ -4,12 +4,19 @@ import android.content.Context;
 
 //import pimp.spatialite_database_driver.*;
 
+//import jsqlite;
+//import pimp.spatialite_database_driver.jsqlite.
+import pimp.spatialite_database_driver.FunConstants;
+
 /**
  * Created by kristina on 9/2/15.
  */
 public class GeoDatabaseHandler {
 
     public GeoDatabaseHandler( Context context, StringBuilder sb ) {
+
+        FunConstants foo = new FunConstants();
+        int temp = foo.getFOOBAR();
 
         Database db = new Database();
 
